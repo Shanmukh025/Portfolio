ace00723f2e0db11a07cad1a0dd7a771a791c425
Shanmukh025 <shanmukh2564@gmail.com>
2024-05-14 17:25:20 +0530

    Commit

--- pages/github.jsx
+++ pages/github.jsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import GitHubCalendar from "react-github-calendar";
 import RepoCard from "../components/RepoCard";
 import styles from "../styles/GithubPage.module.css";
 
@@ -16,35 +14,35 @@ const GithubPage = ({ repos, user }) => {
         <>
             <div className={styles.user}>
                 <div>
-                    <Image
+                    {/* <Image
                         src={user.avatar_url}
                         className={styles.avatar}
                         alt={user.login}
                         width={50}
                         height={50}
-                    />
-                    <h3 className={styles.username}>{user.login}</h3>
+                    /> */}
+                    {/* <h3 className={styles.username}>{user.login}</h3> */}
                 </div>
                 <div>
-                    <h3>{user.public_repos} repos</h3>
+                    <h3>{user.public_repos}A Little Bit About Myself</h3>
                 </div>
-                <div>
-                    <h3>{user.followers} followers</h3>
-                </div>
+                {/* <div>
+                    <h3>{user.followers} </h3>
+                </div> */}
             </div>
             <div className={styles.container}>
                 {repos.map((repo) => (
                     <RepoCard key={repo.id} repo={repo} />
                 ))}
             </div>
-            <div className={styles.contributions}>
+            {/* <div className={styles.contributions}>
                 <GitHubCalendar
                     username={process.env.NEXT_PUBLIC_GITHUB_USERNAME}
                     theme={theme}
                     hideColorLegend
                     hideMonthLabels
                 />
-            </div>
+            </div> */}
         </>
     );
 };
