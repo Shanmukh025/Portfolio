319d06ff34c32031e713bc9278ec1d824e3968ab
Shanmukh025 <shanmukh2564@gmail.com>
2024-05-17 17:44:13 +0530

    Commit

--- pages/github.jsx
+++ pages/github.jsx
@@ -1,14 +1,6 @@
 import styles from "../styles/GithubPage.module.css";
 
 const GithubPage = ({ repos, user }) => {
-    const theme = {
-        level0: "#161B22",
-        level1: "#0e4429",
-        level2: "#006d32",
-        level3: "#26a641",
-        level4: "#39d353",
-    };
-
     return (
         <>
             <div className={styles.user}>
